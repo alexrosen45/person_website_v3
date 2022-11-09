b35609360f3648e1b8fcae1204d00f4aaa368ca9
alex <alexanderrosen45@gmail.com>
2022-11-09 17:37:16 -0500

    about section

--- personal_website-v3/src/components/about/about.jsx
+++ personal_website-v3/src/components/about/about.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import './about.css'
+import Photo from '../../assets/me-about.jpg'
 
 const about = () => {
     return (
-        <div>
-            about
-        </div>
+        <section id='about'>
+            <br />
+            <br />
+            <h2>About Me</h2>
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={Photo} alt="about image" />
+                    </div>
+                </div>
+                <div className="about__content">
+                    <div className="about__cards">
+                        <a href="https://www.linkedin.com/in/alex-rosen-7152281b7/">
+                            <article className='about__card'>
+                                <h5>Experience</h5>
+                                <small>3+ years</small>
+                            </article>
+                        </a>
+                        <a href="https://arpersonalwebsite.com/">
+                            <article className='about__card'>
+                                <h5>Previous Website</h5>
+                                <small>Developed early 2022</small>
+                            </article>
+                        </a>
+                    </div>
+                    <p>
+                        I'll begin studying computer science at the University of Toronto this September, and I'm currently working as a software engineering intern. However, this summer, I will also try to focus a great deal of my time on hackathons and preparing for University courses. The area of computer science that, for now, interests me the most is artificial intelligence. More specifically, the various applications of artificial intelligence to understanding financial markets. Have fun exploring my projects, and feel free to contact me below if you have questions about my resume or proposals for interesting open-source projects.
+                    </p>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- personal_website-v3/src/components/nav/nav.jsx
+++ personal_website-v3/src/components/nav/nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import { AiOutlineHome } from 'react-icons/ai'
+import { MdOutlinePersonPin } from 'react-icons/md'
+import { MdWorkOutline } from 'react-icons/md'
+import { AiOutlineBarChart } from 'react-icons/ai'
+import { AiOutlineContacts } from 'react-icons/ai'
 
 const nav = () => {
     return (
-        <div>nav</div>
+        <nav>
+            <a href="#"><AiOutlineHome size={20} /></a>
+            <a href="#about"><MdOutlinePersonPin size={20} /></a>
+            <a href="#experience"><MdWorkOutline size={20} /></a>
+            <a href="#portfolio"><AiOutlineBarChart size={20} /></a>
+            <a href="#contact"><AiOutlineContacts size={20} /></a>
+        </nav>
     )
 }
 
