ffe777acbe9b6cbee5ddc27bd3af348d6d7bd94a
alex <alexanderrosen45@gmail.com>
2022-11-09 19:16:52 -0500

    remove services

--- personal_website-v3/src/App.jsx
+++ personal_website-v3/src/App.jsx
@@ -3,7 +3,6 @@ import Header from './components/header/header.jsx'
 import Nav from './components/nav/nav.jsx'
 import About from './components/about/about.jsx'
 import Experience from './components/experience/experience.jsx'
-import Serivces from './components/services/services.jsx'
 import Portfolio from './components/portfolio/portfolio.jsx'
 import Footer from './components/footer/footer.jsx'
 import Testimonials from './components/testimonials/testimonials.jsx'
@@ -16,7 +15,6 @@ const App = () => {
             <Nav />
             <About />
             <Experience />
-            <Serivces />
             <Portfolio />
             <Testimonials />
             <Contact />
