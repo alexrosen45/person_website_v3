24980cdf6228b04adedc5e9ff13d1a8ccdf03cb2
alex <alexanderrosen45@gmail.com>
2022-11-11 00:53:21 -0500

    portfolio items in array

--- personal_website-v3/src/components/portfolio/portfolio.jsx
+++ personal_website-v3/src/components/portfolio/portfolio.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 import './portfolio.css'
 import Image1 from '../../assets/portfolio4.jpg'
 
+// portfolio item data
+const PORTFOLIO_ITEMS = [
+    {
+        image: Image1,
+        title: "This is a title",
+        github_link: "",
+        is_demo: false,
+        demo_link: ""
+    },
+    {
+        image: Image1,
+        title: "This is a title with demo",
+        github_link: "",
+        is_demo: true,
+        demo_link: ""
+    }
+]
+
 const portfolio = () => {
     return (
         <section id='portfolio'>
@@ -9,18 +27,34 @@ const portfolio = () => {
             <h2>Portfolio</h2>
 
             <div className="container portfolio__container">
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                        <img src={Image1} alt="portfolio-image-1" />
-                    </div>
-                    <h3>Portfolio item title</h3>
-                    <div className="portfolio__buttons">
-                        <a href="" className='btn'>Github</a>
-                        <a href="" className='btn btn-primary'>Demo</a>
-                    </div>
-                </article>
+                {
+                    PORTFOLIO_ITEMS.map(({
+                        image, title, github_link, is_demo, demo_link
+                    }) => {
+                        var demo_button;
+
+                        if (is_demo) {
+                            demo_button = <a href={demo_link} className='btn btn-primary'>Demo</a>;
+                        } else {
+                            demo_button = null; // show no demo button
+                        }
+
+                        return (
+                            <article className="portfolio__item">
+                                <div className="portfolio__item-image">
+                                    <img src={image} alt="portfolio-image-1" />
+                                </div>
+                                <h3>{title}</h3>
+                                <div className="portfolio__buttons">
+                                    <a href={github_link} className='btn'>Github</a>
+                                    {demo_button}
+                                </div>
+                            </article>
+                        )
+                    })
+                }
             </div>
-        </section>
+        </section >
     )
 }
 
